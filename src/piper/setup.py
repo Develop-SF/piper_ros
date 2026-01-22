f6d22c41332f312b16b4e102627db5be1a2fb9e8
Yen-SF <yenchung.c@shinfangglobal.com>
2026-01-22 11:52:22 +0800

    Feat(piper): add cmd arbiter node
    Signed-off-by: Yen-SF <yenchung.c@shinfangglobal.com>

--- src/piper/setup.py
+++ src/piper/setup.py
@@ -30,6 +30,7 @@ setup(
             'piper_single_ctrl = piper.piper_ctrl_single_node:main',
             'piper_read_slave_joint = piper.piper_read_slave_joint:main',
             'piper_read_leader_joint = piper.piper_read_leader_joint:main',
+            'piper_cmd_arbiter = piper.piper_cmd_arbiter:main',
         ],
     },
 )
